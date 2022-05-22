0e3610dfa8f7d770191d58b221f7187edbf062d2
Shashank <langishashank@gmail.com>
2022-05-22 20:09:44 +0530

    [Shashank]:Added an implemented to finding the maximum number.

---
+++ Generic/Generic/Generics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class Generics
+    {
+        public int Num1, Num2, Num3;
+        public int data(int Num1, int Num2, int Num3)
+        {
+            if (Num1.CompareTo(Num2) > 0 && Num1.CompareTo(Num3) > 0)
+            {
+                return Num1;
+            }
+            else if (Num2.CompareTo(Num3) > 0 && Num2.CompareTo(Num1) > 0)
+            {
+                return Num2;
+            }
+            else if (Num3.CompareTo(Num1) > 0 && Num3.CompareTo(Num2) > 0)
+            {
+                return Num3;
+            }
+            return Num1;
+
+        }
+    }
+}
---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Generic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Generics Number = new Generics();
+            int MaximumNumber = Number.data(11, 67, 24);
+            Console.WriteLine("Maximum Integer is " + MaximumNumber);
+
+        }
+    }
+}
